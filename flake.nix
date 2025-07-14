97f0d0894a6fcbebc616120d8cd7aefc2e66d5c2
Kishan S Patel <bishan.batel@protonmail.com>
2025-07-14 06:16:54 -0700

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { 
+    self, nixpkgs, flake-utils, ...
+    }: flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      libPath = with pkgs; [ ]; 
+    in {
+
+      devShells = { 
+        default = pkgs.mkShell.override {
+          stdenv = pkgs.clang18Stdenv;
+        } {
+            name = "na-engine";
+            packages = with pkgs; [
+              ninja
+              sccache
+              cmake
+              extra-cmake-modules
+              gdb
+            ];
+
+            buildInputs = with pkgs; [
+              llvmPackages_18.lldb 
+              llvmPackages_18.clang-tools
+              llvmPackages_18.clang
+
+              pkg-config
+            ];
+
+            nativeBuildInputs = with pkgs; [ 
+              llvmPackages_18.clang
+            ] ++ libPath;
+
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libPath;
+          };
+      };
+    });
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::println("Hi");
+	return 0;
+}
